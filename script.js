6d85dc928981a3f48068ab2a36ee5dc6218b0814
TatkaMykhnuk <tatka.myhnuk@gmail.com>
2014-08-25 08:04:25 +0300

    c5

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-﻿/**
- * Created by Tatka on 18.08.14.
- */
-function demoDocument(id, name){
+﻿function demoDocument(id, name){
     var self = this;
-
     self.id = ko.observable(id);
     self.name = ko.observable(name);
-    self.Selected = ko.observable(false);
+    self.isChecked = ko.observable(false);
 }
 function viewModel(){
     var self = this;
 
-    self.epost = ko.observable();
-    self.Theme = ko.observable();
-    self.message = ko.observable();
+    self.epost = ko.observable().extend({required: true});
+    self.theme = ko.observable().extend({required: true});
+    self.message = ko.observable().extend({required: true, minLength: 10});
 
     self.availableItems = ko.observableArray();
-    self.availableItemIds = ko.observableArray();
-    self.newDocumentList = ko.observableArray();
-	self.sendInformation= ko.observable(false);
-    self.add = function(){
+    self.selectedItems = ko.observableArray();
+	self.isInformationSent= ko.observable(false);
+    self.fillAvailableItems = function(){
         self.availableItems.push(new demoDocument(1, "О компании QAP INT."));
         self.availableItems.push(new demoDocument(2, "Преимущества использования инструмента для Вашего бизнеса."));
         self.availableItems.push(new demoDocument(3, "Перспективы сотрудничества с QAP INT."));
@@ -27,16 +22,14 @@ function viewModel(){
         self.availableItems.push(new demoDocument(5, "Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(русский)."));
         self.availableItems.push(new demoDocument(6, "Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(english)."));
     };
-    self.showDocument = function (item) {
-        item.Selected(!(item.Selected()));
-    };
+
     self.sendMessage = function(){
-	this.sendInformation(true);
+		this.isInformationSent(true);
     }
 
  }
 
 var viewModel = new viewModel();
 ko.applyBindings(viewModel);
-viewModel.add();
+viewModel.fillAvailableItems();
 
